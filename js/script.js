5e1a2bd411b5e5a94b5d0e26ca351563b75935b9
Dannyngve5 <deparragz@gmail.com>
2024-05-06 10:41:36 -0500

    Añadido sección productos

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const navElements = document.querySelector('.nav-elements');
     const navLinks = document.querySelectorAll('.nav-elements a'); // Selección de todos los enlaces dentro del menú
 
-    // Función para toggle del menú
+    // Toggle del menú
     hamburguerIcon.addEventListener('click', function() {
         navElements.classList.toggle('active');
     });
 
-    // Función para cerrar el menú al hacer clic en un enlace
+    // Cerrar el menú al hacer clic en un enlace
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
             if (navElements.classList.contains('active')) {
@@ -17,12 +17,31 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Función para cerrar el menú si el clic es fuera
+    // Cerrar el menú si el clic es fuera
     document.addEventListener('click', function(event) {
         if (!navElements.contains(event.target) && !hamburguerIcon.contains(event.target)) {
             if (navElements.classList.contains('active')) {
                 navElements.classList.remove('active');
             }
         }
+    });
+
+    // Controladores para los botones de cantidad
+    document.querySelectorAll('.plus').forEach((plus, index) => {
+        plus.addEventListener('click', () => {
+            const num = document.querySelectorAll('.num')[index];
+            let value = parseInt(num.innerText, 10);
+            value++;
+            num.innerText = value;
+        });
+    });
+
+    document.querySelectorAll('.minus').forEach((minus, index) => {
+        minus.addEventListener('click', () => {
+            const num = document.querySelectorAll('.num')[index];
+            let value = parseInt(num.innerText, 10);
+            if (value > 0) value--;
+            num.innerText = value;
+        });
     });
 });
